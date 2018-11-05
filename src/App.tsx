7f6820368c4d2ab78b3d033fc7af0f9ff6dd1415
Bhuwan Ojha <6823387+bhuwanoza@users.noreply.github.com>
2018-11-05 18:50:31 +0545

    Final logo and Background Color Intro Page (#6)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import * as React from "react";
 import "./App.css";
-import intro from "./images/intro.png";
+import logo from "./images/world-logo.png";
 
 class App extends React.Component {
     public render() {
-        return (
-            <div className="App">
-                <img src={intro} className="App-intro" alt={intro}/>
-                <div className="enter-button">
-                    <button className="btn enter-world">Enter World<i className="glyphicon glyphicon-arrow-right arrow"/></button>
-                </div>
-            </div>
-        );
-    }
+    return (
+    <div className="App">
+    <img src={logo} className="App-logo" alt={logo}/>
+    <div className="enter-button">
+    <button className="btn enter-world">Enter World
+        <i className="glyphicon glyphicon-arrow-right arrow"/></button>
+    </div>
+    </div>
+);
+}
 }
 
 export default App;
