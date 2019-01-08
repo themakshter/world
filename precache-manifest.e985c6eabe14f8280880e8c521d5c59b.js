335d07335e45588e5673538686caac2ff9d35289
themakshter <mohammad_ali93@live.com>
2019-01-08 00:07:38 +0000

    Updates

---
+++ precache-manifest.e985c6eabe14f8280880e8c521d5c59b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6bb236a9844a468ccfb66db406abcbd",
+    "url": "/world/static/media/animal-cheetah.e6bb236a.png"
+  },
+  {
+    "revision": "6e4b4e35fe8323bfc631",
+    "url": "/world/static/css/main.642cacc9.chunk.css"
+  },
+  {
+    "revision": "cd703b760bd1319c79d9",
+    "url": "/world/static/js/1.cd703b76.chunk.js"
+  },
+  {
+    "revision": "0ae1b2e3c8e1d4a2d624",
+    "url": "/world/static/js/runtime~main.0ae1b2e3.js"
+  },
+  {
+    "revision": "8a798987a0ca7d04d78d9b1dc5b5eddc",
+    "url": "/world/static/media/world-logo.8a798987.png"
+  },
+  {
+    "revision": "cbfe55b8cd86f9e2178e302dcc38f12a",
+    "url": "/world/static/media/animal-bear.cbfe55b8.png"
+  },
+  {
+    "revision": "6e4b4e35fe8323bfc631",
+    "url": "/world/static/js/main.6e4b4e35.chunk.js"
+  },
+  {
+    "revision": "284383c4eed641c4b7f7a71ea386d6a3",
+    "url": "/world/static/media/animal-giraffe.284383c4.png"
+  },
+  {
+    "revision": "0c2b888c6d2e97dd63557c6645602b43",
+    "url": "/world/static/media/animal-monkey.0c2b888c.png"
+  },
+  {
+    "revision": "7df508eef6bbaffc8d19e9d4ea90fc3f",
+    "url": "/world/static/media/animal-panther.7df508ee.png"
+  },
+  {
+    "revision": "00d0fac068e762ec22ff03914af0dca6",
+    "url": "/world/static/media/map.00d0fac0.png"
+  },
+  {
+    "revision": "ea9d79de3e94c6e6588e22c971294de9",
+    "url": "/world/index.html"
+  }
+];
